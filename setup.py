a7011489352b9e36fe040ef01c3f71b78414c72c
Antonio Ossa <aaossa@uc.cl>
2016-07-07 20:56:57 -0400

    Personalize setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+requirements = list()
+with open('requirements.txt') as f:
+    requirements = f.read().splitline()
+
+readme = str()
+with open('README.md') as f:
+    readme = f.read().splitline()
+
+setup(name='Flask-Openshift template',
+
+      # PEP 440 -- Version Identification and Dependency Specification
+      version='0.0.1',
+
+      # Project description
+      description='Flask template app for OpenShift',
+      long_description=readme,
+
+      # Author details
+      author='Antonio Ossa',
+      author_email='aaossa@uc.cl',
+
+      # Project details
+      url='https://github.com/aaossa/flask-openshift',
+      license="GNU v3"
+
+      # Project dependencies
+      install_requires=requirements,
+      )
