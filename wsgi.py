2e155372e51f5b0515400c54481bd28053c95990
Antonio Ossa <aaossa@uc.cl>
2016-07-08 04:30:14 -0400

    Fix import and variable naming

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os
-from flask_openshift_template import app
+from flask_openshift_template import app as application
 
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, app)
+    httpd = make_server('localhost', 8051, application)
     httpd.serve_forever()
